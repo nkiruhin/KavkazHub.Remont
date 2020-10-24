bf536c0b878d1d838ad6b1d75a8929022785a727
Николай Кирюхин <nkiruhin@yandex.ru>
2020-10-24 13:08:46 +0400

    add validation

--- src/KavkazHubHack.Remont.Web/Endpoints/Images/Load.cs
+++ src/KavkazHubHack.Remont.Web/Endpoints/Images/Load.cs
@@ -1,11 +1,14 @@
 ﻿using Ardalis.ApiEndpoints;
 using KavkazHub.Remont.Web.Command;
+using KavkazHub.Remont.Web.Models;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using KavkazHub.Remont.Web.Models;
 
 namespace KavkazHub.Remont.Web.Endpoints.Images
 {
@@ -27,7 +30,20 @@ namespace KavkazHub.Remont.Web.Endpoints.Images
         ]
         public override async Task<ActionResult<ClassificationResponse>> HandleAsync([FromForm] LoadImageFileRequest request, CancellationToken cancellationToken = default)
         {
-
+            ///Ограничение на размер файла 10МБ
+            if (request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("Message", $"Превышен допустимый размер файла изображения");
+                return BadRequest(ModelState);
+            };
+            ///Проверка расширения файла 10МБ
+            string[] permittedExtensions = { ".jpg", ".png", ".bmp" };
+            var ext = Path.GetExtension(request.File.FileName).ToLowerInvariant();
+            if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
+            {
+                ModelState.AddModelError("Message", $"Не верный формат файла");
+                return BadRequest(ModelState);
+            }
             var response = await _mediator.Send(request, cancellationToken);
             return Ok(response);
         }
